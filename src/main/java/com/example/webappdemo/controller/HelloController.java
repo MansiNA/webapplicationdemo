4eaddcb8b025a9c9a02b8a59d1d821bce0a50b2c
MansiNA <mansikavathiya1998@gmail.com>
2024-03-11 12:32:19 +0100

    first commit

---
+++ src/main/java/com/example/webappdemo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.webappdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to Spring Boot with Thymeleaf!");
+        return "welcome";
+    }
+}
